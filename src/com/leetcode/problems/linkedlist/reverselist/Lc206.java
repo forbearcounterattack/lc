3fc6e6f53e30b2900a40580215fc6fa12b5bd553
forbearcounterattack <hhxxttxs202012@126.com>
2022-03-19 00:20:33 +0800

    lc206

---
+++ src/com/leetcode/problems/linkedlist/reverselist/Lc206.java
@@ -0,0 +1,54 @@
+package com.leetcode.problems.linkedlist.reverselist;
+
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @Author Duole
+ * @Version 1.0
+ */
+public class Lc206 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(3);
+        ListNode node3 = new ListNode(4);
+        ListNode node4 = new ListNode(5);
+
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+
+//        while (head != null) {
+//            System.out.println(head.val + "   ");
+//            head = head.next;
+//        }
+
+        Solution solution = new Solution();
+
+        ListNode resultNode = solution.reverseList(head);
+        while (resultNode != null) {
+            System.out.println(resultNode.val + "   ");
+            resultNode = resultNode.next;
+        }
+    }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        //当前节点的前一个节点，逆转后的下一个节点
+        ListNode pre = null;
+        //当前节点的下一个节点
+        ListNode curNext = null;
+        //head就代表当前节点
+        while (head != null) {
+            curNext = head.next;
+            head.next = pre;
+            pre = head;
+            head = curNext;
+        }
+        return pre;
+    }
+}
---
+++ src/com/leetcode/problems/linkedlist/reverselist/ListNode.java
@@ -0,0 +1,24 @@
+package com.leetcode.problems.linkedlist.reverselist;
+
+/**
+ * @Author Duole
+ * @Version 1.0
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
