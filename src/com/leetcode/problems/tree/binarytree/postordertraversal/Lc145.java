99f88b0e190ecc5d386583d13b6220963ac3538c
forbearcounterattack <hhxxttxs202012@126.com>
2022-03-26 21:35:13 +0800

    add lc144 145

---
+++ src/com/leetcode/problems/tree/binarytree/postordertraversal/Lc145.java
@@ -0,0 +1,74 @@
+package com.leetcode.problems.tree.binarytree.postordertraversal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @Author Duole
+ * @Version 1.0
+ */
+public class Lc145 {
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+
+    //非递归--后序遍历二叉树
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode prev = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.right == null || root.right == prev) {
+                list.add(root.val);
+                prev = root;
+                root = null;
+            } else {
+                stack.push(root);
+                root = root.right;
+            }
+        }
+        return list;
+    }
+
+
+    //递归--后序遍历二叉树
+    public List<Integer> postorderTraversal1(TreeNode root) {
+        List<Integer> list = new ArrayList();
+        postOrder1(root, list);
+        return list;
+    }
+
+    private void postOrder1(TreeNode root, List<Integer> list) {
+        if (null == root) {
+            return;
+        }
+        postOrder1(root.left, list);
+        postOrder1(root.right, list);
+        list.add(root.val);
+    }
+}
---
+++ src/com/leetcode/problems/tree/binarytree/preordertraversal/Lc144.java
@@ -0,0 +1,70 @@
+package com.leetcode.problems.tree.binarytree.preordertraversal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @Author Duole
+ * @Version 1.0
+ */
+public class Lc144 {
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+
+    //非递归--前序遍历二叉树
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curNode = root;
+        while (curNode != null || !stack.isEmpty()) {
+            if (curNode != null) {
+                list.add(curNode.val);
+                stack.add(curNode);
+                curNode = curNode.left;
+            } else {
+                TreeNode popNode = stack.pop();
+                curNode = popNode.right;
+            }
+        }
+        return list;
+    }
+
+
+    //递归--前序遍历二叉树
+    public List<Integer> preorderTraversal1(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        preOrder1(root, list);
+        return list;
+    }
+
+    private void preOrder1(TreeNode root, List<Integer> list) {
+        if (null == root) {
+            return;
+        }
+        list.add(root.val);
+        preOrder1(root.left, list);
+        preOrder1(root.right, list);
+    }
+}
