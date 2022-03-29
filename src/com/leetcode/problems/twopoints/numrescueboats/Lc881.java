22e4bb98cd8338e38188f5d583e2ebcebadfd416
forbearcounterattack <hhxxttxs202012@126.com>
2022-03-29 20:56:45 +0800

    add 881

--- src/com/leetcode/problems/twopoints/numrescueboats/Lc881.java
+++ src/com/leetcode/problems/twopoints/numrescueboats/Lc881.java
@@ -1,12 +1,15 @@
 package com.leetcode.problems.twopoints.numrescueboats;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * @Author Duole
  * @Version 1.0
  */
 public class Lc881 {
     public static void main(String[] args) {
-        int[] people = {3, 2, 3, 2, 2};
+        int[] people = {5, 1, 4, 2};
         int limit = 6;
         Solution solution = new Solution();
         System.out.println(solution.numRescueBoats(people, limit));
@@ -15,33 +18,20 @@ public class Lc881 {
 }
 
 class Solution {
+    //使用快慢指针
     public int numRescueBoats(int[] people, int limit) {
         int count = 0;
-        for (int i = 0; i < people.length - 1; i++) {
-            for (int j = 0; j < people.length - i - 1; j++) {
-                if (people[j] < people[j + 1]) {
-                    int temp = people[j];
-                    people[j] = people[j + 1];
-                    people[j + 1] = temp;
-                }
+        int slow = 0;
+        int fast = people.length - 1;
+        Arrays.sort(people);
+        while (slow <= fast) {
+            if (people[slow] + people[fast] <= limit) {
+                slow++;
             }
+            fast--;
+            count++;
         }
-
-
-        boolean flag = false;
-        int step = 1;
-        for (int i = 0; i < people.length - 1; i = i + step) {
-            if (people[i] + people[i + 1] <= limit) {
-                count++;
-                step = 2;
-                flag = true;
-            } else {
-                if (people[i] <= limit) {
-                    count++;
-                    step = 1;
-                }
-            }
-        }
-        return flag ? count : count + 1;
+        return count;
     }
+
 }
