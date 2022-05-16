65c1beccbe6fbd476df54b39d13925b301ac35d4
forbearcounterattack <hhxxttxs202012@126.com>
2022-05-16 10:20:14 +0800

    update 142

--- src/com/leetcode/problems/linkedlist/detectcycle/Lc142.java
+++ src/com/leetcode/problems/linkedlist/detectcycle/Lc142.java
@@ -1,6 +1,7 @@
 package com.leetcode.problems.linkedlist.detectcycle;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class Lc142 {
     public static void main(String[] args) {
@@ -19,16 +20,41 @@ class ListNode {
 }
 
 class Solution {
-    //方法二：使用hash表记录已遍历的节点。如果遍历过程中出现了hash表中已存在的节点，即出现了环
-    public ListNode detectCycle2(ListNode head) {
-        HashSet<ListNode> set = new HashSet<ListNode>();
-        while (null != head) {
-            if (set.contains(head)) {
-                return head;
+    //方法二：快慢指针
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (null != fast) {
+            if (null != fast.next) {
+                fast = fast.next.next;
             } else {
-                set.add(head);
+                return null;
             }
-            head = head.next;
+            slow = slow.next;
+            if (slow == fast) {
+                ListNode cur = head;
+                //从第一次相遇起，慢指针每次挪一步，cur指针从head开始每次也挪动一步，当它们相遇时，相遇点在环口
+                while (cur != slow) {
+                    slow = slow.next;
+                    cur = cur.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+
+    //方法一：使用hash表
+    public ListNode detectCycle1(ListNode head) {
+        Set<ListNode> set = new HashSet<ListNode>();
+        ListNode cur = head;
+        while (null != cur) {
+            if (set.contains(cur)) {
+                return cur;
+            } else {
+                set.add(cur);
+            }
+            cur = cur.next;
         }
         return null;
     }
