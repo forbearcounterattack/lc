2b53ebced22159881220df85851e17014f75d3a2
forbearcounterattack <hhxxttxs202012@126.com>
2022-05-23 22:46:55 +0800

    update 111

--- src/com/leetcode/problems/tree/binarytree/bfs/mindepth/Lc111.java
+++ src/com/leetcode/problems/tree/binarytree/bfs/mindepth/Lc111.java
@@ -29,31 +29,57 @@ class TreeNode {
 }
 
 class Solution {
-    public int minDepth(TreeNode root) {
-        int min = 1;
-        Queue<TreeNode> queue = new LinkedList();
-        if (null != root) {
-            queue.offer(root);
-        } else {
+    //方法一：递归
+    public int minDepth1(TreeNode root) {
+        if (null == root) {
             return 0;
         }
+        if (null == root.left && null == root.right) {
+            return 1;
+        }
+        int result = Integer.MAX_VALUE;
+        if (null != root.left) {
+            int temp = minDepth(root.left);
+            if (temp < result) {
+                result = temp;
+            }
+        }
+        if (null != root.right) {
+            int temp = minDepth(root.right);
+            if (temp < result) {
+                result = temp;
+            }
+        }
+        return result + 1;
+    }
+
+    //方法二：层序遍历
+    public int minDepth(TreeNode root) {
+        int result = 0;
+        if (null == root) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList();
+        queue.offer(root);
         while (!queue.isEmpty()) {
             int size = queue.size();
+            if (size > 0) {
+                result++;
+            }
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
-                if (node.left == null && node.right == null) {
-                    return min;
-                } else {
-                    if (node.left != null) {
-                        queue.offer(node.left);
-                    }
-                    if (node.right != null) {
-                        queue.offer(node.right);
-                    }
+                //左右孩子都为null,说明该节点处于当前二叉树最浅的层
+                if (null == node.left && null == node.right) {
+                    return result;
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
                 }
             }
-            min += 1;
         }
-        return min;
+        return result;
     }
 }
